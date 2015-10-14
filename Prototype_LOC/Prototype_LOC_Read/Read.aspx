9efb3fcd74c4d871a9421066540fc59814064b1d
proliuxz <A0136010A@BOR21007.iss.nus.edu.sg>
2015-10-14 14:40:58 +0800

    Create 2 new protetype

---
+++ Prototype_LOC/Prototype_LOC_Read/Read.aspx
@@ -0,0 +1,17 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Read.aspx.cs" Inherits="Prototype_LOC_Read.Read" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+</head>
+<body>
+    <form id="form1" runat="server">
+    <div>
+        <asp:Button ID="start" OnClick="start_Click"  runat="server"/>
+        <pre id="inc"></pre>
+    </div>
+    </form>
+</body>
+</html>
---
+++ Prototype_LOC/Prototype_LOC_Read/Read.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Prototype_LOC_Read
+{
+    public partial class Read : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void start_Click(object sender, EventArgs e)
+        {
+            while(true)
+            {
+                String send = DateTime.Now.ToString();
+                
+            }
+        }
+    }
+}
