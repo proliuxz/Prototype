fc56a20ad7d8a3522767c1e5dc1d2427bd6f7035
proliuxz <A0136010A@BOR21007.iss.nus.edu.sg>
2015-10-14 16:48:45 +0800

    Read

---
+++ Prototype_LOC/Read/Loc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Read
+{
+    /**
+     * Entity Class for Read
+     * 
+     * **/
+    public class Loc
+    {
+        public string user_id{set;get;}
+        public double lat{set;get;}
+        public double lon{set;get;}
+        public DateTime dt{set;get;}
+
+        public Loc(String user_id, double lat, double lon, DateTime dateTime)
+        {
+            this.user_id = user_id;
+            this.lat = lat;
+            this.lon = lon;
+            this.dt = dateTime;
+        }
+    }
+}
--- Prototype_LOC/Read/Program.cs
+++ Prototype_LOC/Read/Program.cs
@@ -3,13 +3,130 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MySql.Data.MySqlClient;
+using System.Web.Script.Serialization;
 
 namespace Read
 {
+    /**
+     * This class is used to read data from the database (Table: latest_loc). And praint them to the Console.
+     * 
+     * For 5000 records, less than 1s.
+     **/
     class Program
     {
+        private MySqlConnection connection;
+        private string server;
+        private string database;
+        private string uid;
+        private string password;
+        private string port;
+
+        public void Initialize(string server, string database, string uid, string password, string port)
+        {
+            //server = "localhost";  
+            //database = "connectcsharptomysql";  
+            //uid = "username";  
+            //password = "password";  
+            this.server = server;
+            this.uid = uid;
+            this.password = password;
+            this.port = port;
+            this.database = database;
+            string connectionString = "Data Source=" + server + ";" + "port=" + port + ";" + "Database=" + database + ";" + "User Id=" + uid + ";" + "Password=" + password + ";" + "CharSet = utf8";
+            connection = new MySqlConnection(connectionString);
+        }
+
+        public bool OpenConnection()
+        {
+            try
+            {
+                connection.Open();
+                return true;
+            }
+            catch (MySqlException ex)
+            {  
+                switch (ex.Number)
+                {
+                    case 0:
+                        break;
+
+                    case 1045:
+                        break;
+                }
+                return false;
+            }
+        }
+
+
+        public bool CloseConnection()
+        {
+            try
+            {
+                connection.Close();
+                return true;
+            }
+            catch (MySqlException ex)
+            {
+    
+                return false;
+            }
+        }
+
+        public List<Loc> Select()
+        {
+            string query = "SELECT * FROM latest_loc";
+
+            //Create a list to store the result  
+            List<Loc> list = new List<Loc>();
+ 
+            //Open connection  
+            if (this.OpenConnection() == true)
+            {
+                //Create Command  
+                MySqlCommand cmd = new MySqlCommand(query, connection);
+                //Create a data reader and Execute the command  
+                MySqlDataReader dataReader = cmd.ExecuteReader();
+
+                //Read the data and store them in the list  
+                while (dataReader.Read())
+                {
+                    String user_id =(String) dataReader["user_id"];
+                    double lat = (double)dataReader["lat"];
+                    double lon = (double)dataReader["lon"];
+                    DateTime timestamp = (DateTime)dataReader["timestamp"];
+                    Loc loc = new Loc(user_id, lat,lon,timestamp);
+                    list.Add(loc);
+                }
+                //close Data Reader  
+                dataReader.Close();
+                //close Connection  
+                this.CloseConnection();
+                //return list to be displayed  
+                return list;
+            }
+            else
+            {
+               return list;
+            }
+        }
         static void Main(string[] args)
         {
+            Program program = new Program();
+            program.Initialize("10.10.1.36", "phoenix", "phoenix", "password", "3306");
+            while (true)
+            {
+                List<Loc> result = program.Select();
+                Console.WriteLine(DateTime.Now.ToString());
+                foreach (Loc loc in result)
+                {
+                    Console.WriteLine(loc.user_id + ";" + loc.lat + ";" + loc.lon + ";" + loc.dt.ToString());
+                }
+                Console.WriteLine(DateTime.Now.ToString());
+                program.CloseConnection();
+                System.Threading.Thread.Sleep(5000);
+            }
+            
         }
     }
 }
