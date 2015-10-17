e2eff40cd17dcff02db9e475a674ff68378efa31
proliuxz <345832454@qq.com>
2015-10-17 18:05:54 +0800

    stopwatch

---
+++ Prototype_LOC/Insert/SW.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Insert
+{
+    public class SW
+    {
+        private Stopwatch sw;
+        public SW()
+        {
+            sw = new Stopwatch();
+        }
+        public void start()
+        {
+            sw.Start();
+        }
+        public void end()
+        {
+            sw.Stop();
+        }
+        public decimal getTime()
+        {
+            return sw.ElapsedMilliseconds;
+        }
+        public void reset()
+        {
+            sw.Reset();
+        }
+
+    }
+}
